1abd49864a7b52042fc9a20f30b4886051a3ebe1
xijisz <qq75735889@gmail.com>
2024-04-26 23:55:53 +0800

    1

--- mtproxy.sh
+++ mtproxy.sh
@@ -448,7 +448,7 @@ info_mtp() {
 		gzip -d gost-linux-amd64-2.11.1.gz
 		mv gost-linux-amd64-2.11.1  gost
 		chmod 777 gost
-		./gost -L=mtls://:8443/127.0.0.1:443 >/dev/null 2>&1 &
+		gost -L=mtls://:8443/127.0.0.1:443 >/dev/null 2>&1 &
 		echo -e "配置完成!"
 		
 		
